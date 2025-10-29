acfc1ce1b3b95a1d299d582b2baca5c01fb9d75b
Nihad ALiyev <lnihad257@gmail.com>
2025-10-29 22:31:00 +0400

    Cookies local storage

---
+++ Cookies_local_storage/src/index.js
@@ -0,0 +1,16 @@
+function setCookies() {
+  const firstname = document.getElementById('firstname').value;
+  const email = document.getElementById('email').value;
+  document.cookie = `firstname=${firstname}`;
+  document.cookie = `email=${email}`;
+}
+
+function showCookies() {
+  const p = document.createElement('p');
+  p.innerHTML = `Cookies: ${document.cookie}`;
+  document.body.appendChild(p);
+}
+
+// Bu iki funksiyanı qlobal eləyirik ki, HTML-dən çağırılsın
+window.setCookies = setCookies;
+window.showCookies = showCookies;
